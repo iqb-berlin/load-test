0f169eb5f805010ad91bc8b5a1cf7e6872d49ef4
Nam Du <chi.nam.du@iqb.hu-berlin.de>
2025-01-28 17:18:06 +0100

    give clearer picture of error logs in summary.sh

--- go/summary.sh
+++ go/summary.sh
@@ -4,6 +4,7 @@
 red='\033[0;91m'
 green='\033[0;32m'
 yellow='\033[0;33m'
+grey='\033[0;37m'
 # Clear the color after that
 clear='\033[0m'
 
@@ -15,26 +16,38 @@ echo "Run users: $1"
 error_count=$(grep "ERROR" $2 | wc -l)
 if [ ! $error_count -eq 0 ] ; then
     echo -e "${red}Error Count: $error_count${clear}"
-    error_400=$(grep "WARNING" $2 | grep "400 Bad Request" | wc -l)
+    error_400=$(grep "ERROR" $2 | grep "Response: 400" | wc -l)
     echo " Error Code 400: $error_400"
-    error_404=$(grep "WARNING" $2 | grep "404 Not Found" | wc -l)
+    error_401=$(grep "ERROR" $2 | grep "Response: 401" | wc -l)
+    echo " Error Code 401: $error_401"
+    error_403=$(grep "ERROR" $2 | grep "Response: 403" | wc -l)
+    echo " Error Code 403: $error_403"
+    error_404=$(grep "ERROR" $2 | grep "Response: 404" | wc -l)
     echo " Error Code 404: $error_404"
-    error_500=$(grep "WARNING" $2 | grep "500 Internal Server Error" | wc -l)
+    error_429=$(grep "ERROR" $2 | grep "Response: 429" | wc -l)
+    echo " Error Code 429: $error_429"
+    error_500=$(grep "ERROR" $2 | grep "Response: 500" | wc -l)
     echo " Error Code 500: $error_500"
-    error_502=$(grep "WARNING" $2 | grep "502 Bad Gateway" | wc -l)
+    error_502=$(grep "ERROR" $2 | grep "Response: 502" | wc -l)
     echo " Error Code 502: $error_502"
-    error_504=$(grep "WARNING" $2 | grep "504 Gateway Timeout" | wc -l)
+    error_503=$(grep "ERROR" $2 | grep "Response: 503" | wc -l)
+    echo " Error Code 503: $error_503"
+    error_504=$(grep "ERROR" $2 | grep "Response: 504" | wc -l)
     echo " Error Code 504: $error_504"
-    error_timeout=$(grep "WARNING" $2 | grep "Timeout; Retries left: 0" | wc -l)
-    echo " Error Timeout: $error_timeout"
+    error_timeout=$(grep "ERROR" $2 | grep "Client Timeout" | wc -l)
+    echo " Error Client Timeout: $error_timeout"
 fi
 
 success_count=$(grep "SUCCESS" $2 | wc -l)
 echo -e "${green}Successful: $success_count${clear}"
 
-retries_needed=$(grep "WARNING" $2 | grep "Retries left" | wc -l)
-if [ ! $retries_needed -eq 0 ] ; then
-  echo -e "${yellow}Retries needed: $retries_needed${clear}"
+automatic_retries_needed=$(grep "WARNING" $2 | grep "Automatic retries left" | wc -l)
+if [ ! $automatic_retries_needed -eq 0 ] ; then
+  echo -e "${grey}Automatic Retries nedded for ressource loading: $automatic_retries_needed${clear}"
+fi
+manual_retries_needed=$(grep "WARNING" $2 | grep "Manual retries left" | wc -l)
+if [ ! $manual_retries_needed -eq 0 ] ; then
+  echo -e "${yellow}Manual Retries needed: $manual_retries_needed${clear}"
 fi
 
 echo
