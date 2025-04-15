d49ee46ba545790b9bb309998aeed6159c03d9f5
Nam Du <chi.nam.du@iqb.hu-berlin.de>
2025-04-15 10:05:39 +0200

    migrate go script to locust

--- locust/README.md
+++ locust/README.md
@@ -1,13 +1,14 @@
 # Preparation
 
-- Configure test parameters in load-test-config.yml
+- Configure test parameters in config.json
   - This includes valid URL paths for all unit and resource files
-  - Use the url-list-gen.py script in the root of the project to generate the list
+    (- Use the url-list-gen.py script in the root of the project to generate the list)
 
 *When testing the Testcenter version with file service, you need to change the path for resource files.
 Swap the commented line in the script.*
 
 # Running Locust
+- First install all dependencies with `pip install -r requirements.txt`
 - Run `locust` and open the UI on Port 8089
 
 *When testing a lot of users you may want to run several Locust-workers. Those can run on the same machine.
--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,54 +1,127 @@
 import json
-import yaml
-from locust import HttpUser, task, constant, events
+from jsonschema import validate
+from locust import HttpUser, task, run_single_user
+
+config_schema = {
+    "type": "object",
+    "properties": {
+        "hostname": {"type": "string"},
+        "username": {"type": "string"},
+        "password": {"type": "string"},
+        "increment_user_id": {"type": "boolean"},
+        "booklet_name": {"type": "string"},
+        "workspace": {"type": "string"},
+        "resource_dir": {"type": "string"},
+        "retries": {"type": "number"},
+        "timeout": {"type": "number"},
+        "file_service_mode": {"type": "boolean"}
+    }
+}
 
 config = {}
-
-
-@events.init.add_listener
-def on_locust_init(environment, **_kwargs):
-    load_config_file()
+resource_list = []
+unit_list = []
+global_id = 1
 
 
 def load_config_file():
-    global config
-    with open('load-test-config.yml', 'r') as file:
-        config = yaml.safe_load(file)
+    global config # Declare config as global to modify it in this scope, for reading not necessary
+    global resource_list
+    global unit_list
 
+    with open('config.json', 'r') as file:
+        config = json.load(file)
+        validate(config, config_schema)
+    with open(config['resource_dir'] + 'resources.txt', 'r') as file:
+        for line in file:
+            resource_list.append(line.rstrip())
+    with open(config['resource_dir'] + 'units.txt', 'r') as file:
+        for line in file:
+            unit_list.append(line.rstrip())
+
+
+load_config_file()  # call this function on module level for class to attribute "host" to access it at class definition time
+
+
+# timing of this code:
+# -> module level code definitions and function calls
+# -> class definition (w/ class attribute initial values are calculated here)
+# -> locust @events and their hooks
+# -> class instantiation (w/ instance attribute values are calculated here, when changed in __init__)
 
 class QuickstartUser(HttpUser):
-    wait_time = constant(10)
+    host = config['hostname']
+    timeout = config['timeout']
+    retries = config['retries']
+
+    def __init__(self, *args, **kwargs):
+        global global_id
+        self.id = global_id
+        global_id += 1
+        super().__init__(*args, **kwargs)
 
     @task
     def load_test(self):
+        print('global id ', self.id)
+
         try:
             token, groupToken = self.get_tokens()
         except ValueError:
             return
         headers = {'AuthToken': token}
 
-        with self.client.put('/api/test', data=json.dumps({'bookletName': config['bookletName']}),
-                             headers=headers, catch_response=True) as response:
-            if response.status_code == 504:
-                response.failure("Timeout on Test start (504)")
-                return
+        with self.client.put('/api/test', data=json.dumps({'bookletName': config['booklet_name']}),
+                             headers=headers, timeout=self.timeout, catch_response=True) as response:
+            if response.status_code >= 400:
+                response.failure("Error of some kind")
             else:
                 test_number = response.content.decode("utf-8")
+                response.success()
 
-        self.client.get("/api/test/" + test_number, headers=headers)
+        with (self.client.get("/api/test/" + test_number, headers=headers, timeout=self.timeout, catch_response=True)
+              as response):
+            if response.status_code >= 400:
+                response.failure("Error of some kind")
+            else:
+                response.success()
 
-        for file in config['UNIT_URLS']:
-            self.client.get('/api/test/' + test_number + '/unit/' + file, headers=headers, name='Unit: ' + file)
+        for file in unit_list:
+            with self.client.get('/api/test/' + test_number + '/unit/' + file, headers=headers, name='Unit: ' + file,
+                                 timeout=self.timeout, catch_response=True) as response:
+                if response.status_code >= 400:
+                    response.failure("Error of some kind")
+                else:
+                    response.success()
 
-        for file in config['RESOURCE_FILES']:
-#             self.client.get('/api/test/' + test_number + '/resource/' + file, headers=headers, name=file)
-            self.client.get('/fs/file/' + groupToken + '/ws_1/Resource/' + file, headers=headers, name='Resource: ' + file)
+        for file in resource_list:
+            workspace = config['workspace']
+            if config['file_service_mode'] is False:
+                self.client.get('/api/test/' + test_number + '/resource/' + file, headers=headers, name=file,
+                                timeout=self.timeout)
+            else:
+                with self.client.get(f"/fs/file/{groupToken}/{workspace}/Resource/{file}", headers=headers,
+                                     name='Resource: ' + file, timeout=self.timeout, catch_response=True) as response:
+                    if response.status_code >= 400:
+                        response.failure("Error of some kind")
+                    else:
+                        response.success()
 
     def get_tokens(self) -> tuple[str, str]:
-        auth = json.dumps(config['auth'])
-        with self.client.put('/api/session/login', data=auth, catch_response=True) as response:
-            if response.status_code == 504:
-                response.failure("Timeout: Getting token failed! (504)")
-                raise ValueError
+        username = config['username']
+        if config['increment_user_id'] is True:
+            username += str(self.id)
+
+        data = json.dumps({'name': username, 'password': config['password']})
+
+        with self.client.put('/api/session/login', data=data, name='putSessionLogin', timeout=self.timeout,
+                             catch_response=True, max_retries=2) as response:
+            if response.status_code >= 400:
+                response.failure("Some error occurred")
             else:
-                return response.json()['token'], response.json()['groupToken']
+                response.success()
+
+        return response.json()['token'], response.json()['groupToken']
+
+
+if __name__ == "__main__":
+    run_single_user(QuickstartUser)
